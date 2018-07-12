64b26b24e2ffa8797d19ba24d5e81be315ab7966
neertel <35453123+neertel@users.noreply.github.com>
2018-07-12 04:16:31 +0000

    Update apache_stop.sh

--- scripts/apache_stop.sh
+++ scripts/apache_stop.sh
@@ -2,3 +2,6 @@
  
 #Stop apache service before deploying new code
 service apache2 stop
+
+tar -vczf /opt/codedeploy-agent/deployment-root/8c9cf9c2-f1e0-4b37-867b-3e3ffbfdd562/ /home/ubuntu/code/backup`date`.tar.gz
+rm -rf /opt/codedeploy-agent/deployment-root/8c9cf9c2-f1e0-4b37-867b-3e3ffbfdd562/* 
