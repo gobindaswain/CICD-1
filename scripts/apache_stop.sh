a30cd6ea57126dd7cf0d60dc86a7f916c439caaa
neertel <35453123+neertel@users.noreply.github.com>
2018-07-12 04:13:01 +0000

    Update apache_stop.sh

--- scripts/apache_stop.sh
+++ scripts/apache_stop.sh
@@ -2,7 +2,3 @@
  
 #Stop apache service before deploying new code
 service apache2 stop
- 
-mkdir -p /home/ubuntu/code
-tar -vczf /home/sap/betasite-git-backup.`date`.tar.gz /home/xxuserxx/xxcodedirectoryxx
-#Copy current code to a directory
