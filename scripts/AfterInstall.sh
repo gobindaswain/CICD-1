d08412709890b306bb6e437890e557c71fba331d
neertel <35453123+neertel@users.noreply.github.com>
2018-07-16 06:57:02 +0000

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp -rf /opt/codedeploy-agent/deployment-root/8c9cf9c2-f1e0-4b37-867b-3e3ffbfdd562/*/deployment-archive/index.html /var/www/html/index.html
 service apache2 restart
+echo $uname
